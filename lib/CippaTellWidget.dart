7a12ab18df9680720a68396fb9a242983b2bf5bd
Artvell3000 <86819826+Artvell3000@users.noreply.github.com>
2024-08-19 00:01:25 +0700

    put the Text on the Cippa's Message

--- lib/CippaTellWidget.dart
+++ lib/CippaTellWidget.dart
@@ -9,30 +9,57 @@ class CippaTellWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       children: [
-        Expanded(
+        const Expanded(
           flex:111,
             child: SizedBox()
         ),
         Expanded(
             flex: 842,
-            child: Stack(children: [
+            child: Stack(
+              children: [
               Column(
                 children: [
                   Expanded(
                       flex:403,
-                      child: Image.asset(Assets.cippasMessage)
+                      child:Stack(
+                        fit: StackFit.loose,
+                          children: [
+                            Image.asset(Assets.cippasMessage),
+                            const Column(
+                              children: [
+                                Expanded(
+                                    flex:231,
+                                    child: Padding(padding: EdgeInsets.only(left: 10, top: 5),
+                                    child: SizedBox(
+                                      width: 170,
+                                      height: 30,
+                                      child: Center(
+                                        child: Text("hi"),
+                                      ))
+                                    )
+                                    )
+                                ,
+                                const Expanded(
+                                    flex: 172,
+                                    child: SizedBox()
+                                )
+                              ],
+                            )
+                          ]
+                      )
                   ),
-                  Expanded(
+                  const Expanded(
                       flex:439,
                       child: SizedBox()
                   ),
                 ],
               ),
               Column(
+                //crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Expanded(
+                  const Expanded(
                       flex:296,
                       child: SizedBox()
                   ),
