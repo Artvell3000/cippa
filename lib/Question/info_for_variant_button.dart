44109f39f2a9996efe3457d2869dc7dd22cb22fd
Artvell3000 <86819826+Artvell3000@users.noreply.github.com>
2024-08-20 03:47:48 +0700

    creating information classes for spoons, baths, mugs

--- lib/Question/info_for_variant_button.dart
+++ lib/Question/info_for_variant_button.dart
@@ -22,3 +22,42 @@ class PotInfoForVariantButton extends InfoForVariantButton{
 
   PotInfoForVariantButton(super.onPressed);
 }
+
+class SpoonInfoForVariantButton extends InfoForVariantButton{
+  @override
+  String imgPath = Assets.spoonImgPng;
+
+  @override
+  EdgeInsetsGeometry padding = const EdgeInsets.only(top:0, bottom: 0, left: 0, right: 0);
+
+  @override
+  BoxFit fit = BoxFit.scaleDown;
+
+  SpoonInfoForVariantButton(super.onPressed);
+}
+
+class MugInfoForVariantButton extends InfoForVariantButton{
+  @override
+  String imgPath = Assets.mugImgPng;
+
+  @override
+  EdgeInsetsGeometry padding = const EdgeInsets.only(top:20, bottom: 20, left: 25, right: 10);
+
+  @override
+  BoxFit fit = BoxFit.scaleDown;
+
+  MugInfoForVariantButton(super.onPressed);
+}
+
+class BathInfoForVariantButton extends InfoForVariantButton{
+  @override
+  String imgPath = Assets.bathImgPng;
+
+  @override
+  EdgeInsetsGeometry padding = const EdgeInsets.only(top:0, bottom: 10, left: 0, right: 5);
+
+  @override
+  BoxFit fit = BoxFit.scaleDown;
+
+  BathInfoForVariantButton(super.onPressed);
+}
